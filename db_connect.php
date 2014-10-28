0dad6b5930b7d73c653c550730f49b19e05ed78c
talhaobject90 <talha@object90.com>
2014-10-28 15:30:51 +0530

    sd

---
+++ db_connect.php
@@ -0,0 +1,34 @@
+<?php
+// Connect to database server
+//mysql_connect("localhost", "root", "password") or die (mysql_error ());
+mysql_connect("127.3.13.2", "adminxntQq3s", "s-FSsSggM7q3") or die (mysql_error ());
+
+
+// changed
+
+
+
+// configure table_names;
+$assets_table = 'assets';
+
+// Select database
+mysql_select_db($assets_table) or die(mysql_error());
+
+
+// SQL query
+//$strSQL = "SELECT * FROM assets";
+
+// Execute the query (the recordset $rs contains the result)
+//$rs = mysql_query($strSQL);
+
+// Loop the recordset $rs
+// Each row will be made into an array ($row) using mysql_fetch_array
+//while($row = mysql_fetch_array($rs)) {
+
+	// Write the value of the column FirstName (which is now in the array $row)
+//	echo $row['name'] . "<br />";
+
+//}
+
+// Close the database connection
+//mysql_close();
