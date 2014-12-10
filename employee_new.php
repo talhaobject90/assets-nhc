bcc45ef04b1e2e87655350f049f1990df6e87db3
talhaobject90 <talha@object90.com>
2014-12-10 22:42:52 +0530

    sdf

--- employee_new.php
+++ employee_new.php
@@ -222,7 +222,7 @@ if($Edit_mode){
 	}
 else{
 	echo '<input class="btn btn-primary"  type="submit"  name="save"  value="Save Employee"   style="margin-right:20px;" >';
-	echo '<a href="employee.php"><input class="btn btn-default"  type="button"  value="Cancel" ></a>';
+	echo '<a href="employees.php"><input class="btn btn-default"  type="button"  value="Cancel" ></a>';
 	}
 	
 ?>
