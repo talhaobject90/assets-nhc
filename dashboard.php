ed076a619b7d72aff6d4daec2911ecff28680b11
talhaobject90 <talha@object90.com>
2015-01-14 17:53:35 +0530

    sdf

--- dashboard.php
+++ dashboard.php
@@ -61,65 +61,50 @@
   <?php  echo ($username_not_set?'Set Password':'Change Password') ;?>
   
 </button>
-    <?php  echo $_SERVER['REMOTE_ADDR'];?>
-    <?php  echo detect_city('117.216.2.30')?>
+    
 <br>
 <br>
 <br>
 
 
-
+<div style="display:none;" id="ip_location"> 
+	
 <?php  
 
-function detect_city($ip) {
+include('ip2locationlite.class.php');
 
-	$default = 'UNKNOWN';
+//Load the class
+$ipLite = new ip2location_lite;
+$ipLite->setKey('3c87d46b4b4a52dd2a5f6a4acd3687e94dd33322c913fc5dbe9f2a56827c3454');
 
-	if (!is_string($ip) || strlen($ip) < 1 || $ip == '127.0.0.1' || $ip == 'localhost')
-		$ip = '8.8.8.8';
+//Get errors and locations
+$locations = $ipLite->getCity($_SERVER['REMOTE_ADDR']);
+$errors = $ipLite->getError();
 
-	$curlopt_useragent = 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.2) Gecko/20100115 Firefox/3.6 (.NET CLR 3.5.30729)';
-
-	$url = 'http://ipinfodb.com/ip_locator.php?ip=' . urlencode($ip);
-	$ch = curl_init();
-
-	$curl_opt = array(
-			CURLOPT_FOLLOWLOCATION  => 1,
-			CURLOPT_HEADER      => 0,
-			CURLOPT_RETURNTRANSFER  => 1,
-			CURLOPT_USERAGENT   => $curlopt_useragent,
-			CURLOPT_URL       => $url,
-			CURLOPT_TIMEOUT         => 1,
-			CURLOPT_REFERER         => 'http://' . $_SERVER['HTTP_HOST'],
-	);
-
-	//print_r($curl_opt);
-	curl_setopt_array($ch, $curl_opt);
-
-	$content = curl_exec($ch);
-
-	if (!is_null($curl_info)) {
-		$curl_info = curl_getinfo($ch);
+//Getting the result
+echo "<p>\n";
+echo "<strong>First result</strong><br />\n";
+if (!empty($locations) && is_array($locations)) {
+	foreach ($locations as $field => $val) {
+		echo $field . ' : ' . $val . "<br />\n";
 	}
-
-	curl_close($ch);
-
-	if ( preg_match('{<li>City : ([^<]*)</li>}i', $content, $regs) )  {
-		$city = $regs[1];
-	}
-	if ( preg_match('{<li>State/Province : ([^<]*)</li>}i', $content, $regs) )  {
-		$state = $regs[1];
-	}
-
-	if( $city!='' && $state!='' ){
-		$location = $city . ', ' . $state;
-		return $location;
-	}else{
-		return $default;
-	}
-
 }
+echo "</p>\n";
+
+//Show errors
+echo "<p>\n";
+echo "<strong>Dump of all errors</strong><br />\n";
+if (!empty($errors) && is_array($errors)) {
+	foreach ($errors as $error) {
+		echo var_dump($error) . "<br /><br />\n";
+	}
+} else {
+	echo "No errors" . "<br />\n";
+}
+echo "</p>\n";
 ?>
+
+</div>
 
 
 			 
